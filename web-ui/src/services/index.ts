05d0edeec70a12bccb686d82f9ec93b95493a915
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2025-10-29 20:18:57 +0200

    Give up on developing our own UI, use existing one

--- web-ui/src/services/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Service exports for the transcript editor.
- */
-
-export { apiService } from './api';
-export { transcriptProcessor, TranscriptProcessor } from './transcriptProcessor';
-export { versionManager, VersionManager } from './versionManager';
-export { transcriptVersionsApi } from './transcriptVersionsApi';
